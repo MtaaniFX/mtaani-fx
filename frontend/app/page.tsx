d97c5a79b9b7159ea06134a033054d2ba878abeb
najwang <anyonanicholas86@gmail.com>
2024-12-07 21:00:27 +0300

    feat: make Features section in cards

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,8 @@ import AppTheme from "@/components/internal/shared-theme/AppTheme";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppAppBar from "@/components/internal/mui-template/components/AppAppBar";
 import Hero from "@/components/internal/ui/Hero";
-import ServiceFeatures from "@/components/internal/landing-page/ServiceFeatures";
+// import ServiceFeatures from "@/components/internal/landing-page/ServiceFeatures";
+import Features from "@/components/internal/landing-page/Features";
 import Testimonials from "@/components/internal/landing-page/Testimonials";
 import Pricing from "@/components/internal/landing-page/Pricing";
 import FAQ from "@/components/internal/landing-page/FAQ";
@@ -21,7 +22,8 @@ function LandingPage(props:any) {
             <LandingPageBackground>
                 <AppAppBar />
                 <Hero/>
-                <ServiceFeatures/>
+                <Features/>
+                {/*<ServiceFeatures/>*/}
                 <Testimonials/>
                 <Pricing/>
                 <FAQ/>
