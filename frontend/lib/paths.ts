b49ca5051313677fb03c28e5b8a7e78219499daa
najwang <anyonanicholas86@gmail.com>
2024-12-10 16:25:31 +0300

    feat: define common paths as constants

---
+++ frontend/lib/paths.ts
@@ -0,0 +1,11 @@
+export const paths = {
+  home: '/',
+  auth: { signIn: '/sign-in', signUp: '/sign-up', resetPassword: '/reset-password' },
+  dashboard: {
+    overview: '/dashboard',
+    account: '/dashboard/account',
+    customers: '/dashboard/customers',
+    integrations: '/dashboard/integrations',
+    settings: '/dashboard/settings',
+  },
+} as const;
