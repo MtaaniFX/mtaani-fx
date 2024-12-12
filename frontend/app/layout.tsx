89fd65750131ff09f264fa9673e14f323d93c32b
najwang <anyonanicholas86@gmail.com>
2024-12-12 16:18:48 +0300

    refactor: move child nodes to main html component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,33 +2,31 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { Inter as FontSans } from "next/font/google"
- 
+
 import { cn } from "@/lib/utils"
 import React from "react";
- 
+
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
+    subsets: ["latin"],
+    variable: "--font-sans",
 })
 
 export const metadata: Metadata = {
-  title: "Mtaani FX",
-  description: "Invest in your future",
+    title: "Mtaani FX",
+    description: "Invest in your future",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-    <body
-        className={cn(
-            "min-h-screen bg-background font-sans antialiased",
+export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
+    return (
+        <html lang="en">
+        <body>
+        <main  className={cn(
+            "min-h-screen font-sans antialiased",
             fontSans.variable
-        )}
-    >{children}</body>
-    </html>
-  );
+        )}>
+            {children}
+        </main>
+        </body>
+        </html>
+    );
 }
