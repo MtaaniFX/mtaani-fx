d5804a7ac61d51164edcbe75fdd9747237ea368d
najwang <anyonanicholas86@gmail.com>
2024-12-12 21:12:06 +0300

    feat: improve user authentication

--- frontend/app/(auth)/components/SignUp.tsx
+++ frontend/app/(auth)/components/SignUp.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
-import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
@@ -10,63 +9,34 @@ import FormControl from '@mui/material/FormControl';
 import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import MuiCard from '@mui/material/Card';
-import { styled } from '@mui/material/styles';
-import AppTheme from '@/components/internal/shared-theme/AppTheme';
-import { GoogleIcon, FacebookIcon } from './CustomIcons';
-import ColorModeSelect from '@/components/internal/shared-theme/ColorModeSelect';
+import {FacebookIcon, GoogleIcon} from './CustomIcons';
 import {FaviconRow as SitemarkIcon} from '@/components/internal/icons/Favicon';
+import {MtCard} from "@/components/internal/styled/MtCard";
+import {paths} from "@/lib/paths";
+import {createClient} from "@/utils/supabase/client";
+import { useRouter } from 'next/navigation';
 
-const Card = styled(MuiCard)(({ theme }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    alignSelf: 'center',
-    width: '100%',
-    padding: theme.spacing(4),
-    gap: theme.spacing(2),
-    margin: 'auto',
-    boxShadow:
-        'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-    [theme.breakpoints.up('sm')]: {
-        width: '450px',
-    },
-    ...theme.applyStyles('dark', {
-        boxShadow:
-            'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
-    }),
-}));
+const supabase = createClient();
 
-const SignUpContainer = styled(Stack)(({ theme }) => ({
-    height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
-    minHeight: '100%',
-    padding: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-        padding: theme.spacing(4),
-    },
-    '&::before': {
-        content: '""',
-        display: 'block',
-        position: 'absolute',
-        zIndex: -1,
-        inset: 0,
-        backgroundImage:
-            'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-        backgroundRepeat: 'no-repeat',
-        ...theme.applyStyles('dark', {
-            backgroundImage:
-                'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-        }),
-    },
-}));
+type SignUpProps = {
+    // A dispatcher, that when set to true, will display a dialogue,
+    // telling the user the given action is under construction
+    setUnderConstruction:  React.Dispatch<React.SetStateAction<boolean>>,
+    setSubmissionError:  React.Dispatch<React.SetStateAction<boolean>>,
+    setSubmissionMessage:  React.Dispatch<React.SetStateAction<string>>,
+    setShowEmailConfirmation:  React.Dispatch<React.SetStateAction<boolean>>,
+}
 
-export default function SignUp(props: { disableCustomTheme?: boolean }) {
+export default function SignUp(props: SignUpProps) {
+    const {setUnderConstruction, setSubmissionMessage, setSubmissionError, setShowEmailConfirmation} = props;
     const [emailError, setEmailError] = React.useState(false);
     const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
     const [passwordError, setPasswordError] = React.useState(false);
     const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
     const [nameError, setNameError] = React.useState(false);
     const [nameErrorMessage, setNameErrorMessage] = React.useState('');
+
+    const router = useRouter();
 
     const validateInputs = () => {
         const email = document.getElementById('email') as HTMLInputElement;
@@ -105,130 +75,167 @@ export default function SignUp(props: { disableCustomTheme?: boolean }) {
         return isValid;
     };
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
         if (nameError || emailError || passwordError) {
-            event.preventDefault();
             return;
         }
-        const data = new FormData(event.currentTarget);
-        console.log({
-            name: data.get('name'),
-            lastName: data.get('lastName'),
-            email: data.get('email'),
-            password: data.get('password'),
+
+        const formData = new FormData(event.currentTarget);
+        const {name, email, password} = {
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password'),
+        }
+
+        if (!name || !email || !password) {
+            setSubmissionError(true);
+            setSubmissionMessage("Invalid Credentials");
+            return;
+        }
+
+        const url = new URL(window.location.href);
+        let redirectTo: string | null | undefined = url.searchParams.get("redirect_to");
+        if (!redirectTo) {
+            redirectTo = undefined;
+        }
+
+        console.log("Sending SignUp request: email:", email, " password: ", password);
+        const { data, error } = await supabase.auth.signUp({
+            email: email as string,
+            password: password as string,
+            options: {
+                emailRedirectTo: redirectTo,
+            },
         });
+        console.log("SignUp response: ", error);
+        console.log("SignUp response: data: ", data);
+
+        if (error) {
+            setSubmissionError(true);
+            setSubmissionMessage(error.message);
+            return;
+        }
+
+        if (data.user) {
+            if(data.user.email_confirmed_at){
+                // This user is already a member, redirect to login page
+                let goTo: string = paths.auth.signIn;
+                if (redirectTo){
+                    goTo = `${goTo}/redirect_to=${redirectTo}`;
+                }
+                router.replace(goTo);
+                return;
+            }
+        }
+
+        setShowEmailConfirmation(true);
     };
 
     return (
-        <AppTheme {...props}>
-            <CssBaseline enableColorScheme />
-            <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
-            <SignUpContainer direction="column" justifyContent="space-between">
-                <Card variant="outlined">
-                    <SitemarkIcon />
-                    <Typography
-                        component="h1"
-                        variant="h4"
-                        sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+        <MtCard variant="outlined">
+            <SitemarkIcon/>
+            <Typography
+                component="h1"
+                variant="h4"
+                sx={{width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)'}}
+            >
+                Sign up
+            </Typography>
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
+                sx={{display: 'flex', flexDirection: 'column', gap: 2}}
+            >
+                <FormControl>
+                    <FormLabel htmlFor="name">Full name</FormLabel>
+                    <TextField
+                        autoComplete="name"
+                        name="name"
+                        required
+                        fullWidth
+                        id="name"
+                        placeholder="Jon Doe Snow"
+                        error={nameError}
+                        helperText={nameErrorMessage}
+                        color={nameError ? 'error' : 'primary'}
+                    />
+                </FormControl>
+                <FormControl>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        placeholder="your@email.com"
+                        name="email"
+                        autoComplete="email"
+                        variant="outlined"
+                        error={emailError}
+                        helperText={emailErrorMessage}
+                        color={passwordError ? 'error' : 'primary'}
+                    />
+                </FormControl>
+                <FormControl>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        placeholder="•••••••••"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                        variant="outlined"
+                        error={passwordError}
+                        helperText={passwordErrorMessage}
+                        color={passwordError ? 'error' : 'primary'}
+                    />
+                </FormControl>
+                <FormControlLabel
+                    control={<Checkbox value="allowExtraEmails" color="primary"/>}
+                    label="I want to receive updates via email."
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    onClick={validateInputs}
+                >
+                    Sign up
+                </Button>
+                <Typography sx={{textAlign: 'center'}}>
+                    Already have an account?{' '}
+                    <Link
+                        href={paths.auth.signIn}
+                        variant="body2"
+                        sx={{alignSelf: 'center'}}
                     >
-                        Sign up
-                    </Typography>
-                    <Box
-                        component="form"
-                        onSubmit={handleSubmit}
-                        sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
-                    >
-                        <FormControl>
-                            <FormLabel htmlFor="name">Full name</FormLabel>
-                            <TextField
-                                autoComplete="name"
-                                name="name"
-                                required
-                                fullWidth
-                                id="name"
-                                placeholder="Jon Snow"
-                                error={nameError}
-                                helperText={nameErrorMessage}
-                                color={nameError ? 'error' : 'primary'}
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor="email">Email</FormLabel>
-                            <TextField
-                                required
-                                fullWidth
-                                id="email"
-                                placeholder="your@email.com"
-                                name="email"
-                                autoComplete="email"
-                                variant="outlined"
-                                error={emailError}
-                                helperText={emailErrorMessage}
-                                color={passwordError ? 'error' : 'primary'}
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor="password">Password</FormLabel>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password"
-                                placeholder="•••••••••"
-                                type="password"
-                                id="password"
-                                autoComplete="new-password"
-                                variant="outlined"
-                                error={passwordError}
-                                helperText={passwordErrorMessage}
-                                color={passwordError ? 'error' : 'primary'}
-                            />
-                        </FormControl>
-                        <FormControlLabel
-                            control={<Checkbox value="allowExtraEmails" color="primary" />}
-                            label="I want to receive updates via email."
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            onClick={validateInputs}
-                        >
-                            Sign up
-                        </Button>
-                    </Box>
-                    <Divider>
-                        <Typography sx={{ color: 'text.secondary' }}>or</Typography>
-                    </Divider>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-                        <Button
-                            fullWidth
-                            variant="outlined"
-                            onClick={() => alert('Sign up with Google')}
-                            startIcon={<GoogleIcon />}
-                        >
-                            Sign up with Google
-                        </Button>
-                        <Button
-                            fullWidth
-                            variant="outlined"
-                            onClick={() => alert('Sign up with Facebook')}
-                            startIcon={<FacebookIcon />}
-                        >
-                            Sign up with Facebook
-                        </Button>
-                        <Typography sx={{ textAlign: 'center' }}>
-                            Already have an account?{' '}
-                            <Link
-                                href={""}
-                                variant="body2"
-                                sx={{ alignSelf: 'center' }}
-                            >
-                                Sign in
-                            </Link>
-                        </Typography>
-                    </Box>
-                </Card>
-            </SignUpContainer>
-        </AppTheme>
+                        Sign in
+                    </Link>
+                </Typography>
+            </Box>
+            <Divider>
+                <Typography sx={{color: 'text.secondary'}}>or</Typography>
+            </Divider>
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+                <Button
+                    fullWidth
+                    variant="outlined"
+                    onClick={() => setUnderConstruction(true)}
+                    startIcon={<GoogleIcon/>}
+                >
+                    Sign up with Google
+                </Button>
+                <Button
+                    fullWidth
+                    variant="outlined"
+                    onClick={() => setUnderConstruction(true)}
+                    startIcon={<FacebookIcon/>}
+                >
+                    Sign up with Facebook
+                </Button>
+            </Box>
+        </MtCard>
     );
 }
--- frontend/app/(auth)/sign-in/page.tsx
+++ frontend/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,16 @@
 "use client"
 
-import SignInSide from '../components/SignInSide'
+import SignInCard from '../components/SignInCard'
+import ThemedRootLayout from "@/app/(auth)/components/ThemedRootLayout";
+import Content from "@/app/(auth)/components/Content";
+import ContentBySideCard from "@/app/(auth)/components/ContentBySideCard";
 
-export default SignInSide;
+export default function SignInPage() {
+    return (
+        <ThemedRootLayout>
+            <ContentBySideCard
+                content={<Content/>}
+                card={<SignInCard/>}/>
+        </ThemedRootLayout>
+    )
+};
