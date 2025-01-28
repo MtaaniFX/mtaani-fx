bb0a45da30bf087f0b4e1f13576f53826d35c6d9
rayjonesjay <rayjaymuiruri@gmail.com>
2025-01-29 01:45:28 +0300

    used to register url

---
+++ register_url.py
@@ -0,0 +1,10 @@
+"""
+this script is meant to register the URL endpoints for confirmation and validation
+when a transaction occurs. a transaction here is c2b or stkpush.
+
+this script in production should only be ran once since safaricom expects the two 
+urls to be registered only once.
+
+i am writing it in its own file to make it easier to modify and run
+"""
+
