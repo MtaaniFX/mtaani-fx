59e2fe50d159ba23dad6c7bafd5c18935d1037b0
rayjonesjay <rayjaymuiruri@gmail.com>
2025-01-29 01:45:43 +0300

    main app

--- pythonbackend/main.py
+++ pythonbackend/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
-from starlette import status
-import requests
 from b2c import router as b2c_router
-from models import UserSignup, UserLogin
 from supabase_client import *
-import bcrypt
 from admin import router as admin_router
 from c2b import router as c2b_router
-from daraja_utils import router as stk_router
+from stk import router as stk_router
+from check_ip import check_ip_middleware
 
 app = FastAPI()
+
+# add the middleware to the app
+app.middleware('http')(check_ip_middleware)
 
 # Include the B2C routes
 app.include_router(b2c_router)
@@ -19,7 +19,6 @@ app.include_router(admin_router)
 
 # Include C2B routes
 app.include_router(c2b_router)
-
 
 # Include stk routes
 app.include_router(stk_router)
